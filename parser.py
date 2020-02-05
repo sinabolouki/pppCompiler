ec24fb0304ba9eab1b6ec7519ba8c2fd10130e61
SinaBolouki <sina.bolouki@gmail.com>
2020-02-05 09:34:32 +0330

    parser main structure added

--- parser.py
+++ parser.py
@@ -4,6 +4,37 @@ import csv
 file = open('sampleText.txt')
 
 scanner = scanner.Scanner(file)
-with open('parseTable.csv') as parseTable:
-    csv_reader = csv.reader(parseTable, delimiter = ',')
-    print(csv_reader)
+parse_table_reader = csv.DictReader(open('parseTable.csv', 'r'), delimiter = ',')
+parse_table_list = []
+for row in parse_table_reader:
+    print(row['REAL'])
+    parse_table_list.append(row)
+state = 0
+token = scanner.parseToken()
+parse_stack = []
+
+
+def generate_code(func_name):
+    if func_name == 'NoSem':
+        return
+
+
+
+
+while token.type != 'EOF':
+    table_cell = parse_table_list[state][token.type]
+    tc_splitted = table_cell.split()
+    if tc_splitted[0] == 'REDUCE':
+        next_state = parse_stack.pop()
+        state = next_state
+    elif tc_splitted[0] == 'PUSH_GOTO':
+        parse_stack.append(state)
+        next_state = int(str(list(tc_splitted[1])[1:]))
+        generate_code(tc_splitted[2])
+    elif tc_splitted[0] == 'SHIFT':
+        next_state = int(str(list(tc_splitted[1])[1:]))
+        generate_code(tc_splitted[2])
+    else:
+        print('errror in line filan')
+        break
+    token = scanner.parseToken()
